c3be58113a96e6cf3b46b2312319a4409e6e2608
Kashish Verma <kashishv.1711@gmail.com>
2026-08-19 20:06:36 +0530

    Time: 2 ms (73.68%), Space: 44.1 MB (98.95%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
@@ -1,57 +1,47 @@
 class Solution {
-    public int largestInteger(int[] nums, int k) {
-        int n = nums.length;
-
-        if (k == 1) {
-            HashMap<Integer, Integer> map = new HashMap<>();
-
-            for (int num : nums) {
-                map.put(num, map.getOrDefault(num, 0) + 1);
-            }
-
-            int ans = -1;
-
-            for (int num : map.keySet()) {
-                if (map.get(num) == 1) {
-                    ans = Math.max(ans, num);
-                }
-            }
-
-            return ans;
-        }
-
-        if (k == n) {
-            int ans = 0;
-
-            for (int num : nums) {
-                ans = Math.max(ans, num);
-            }
-
-            return ans;
-        }
-
-        int ans = -1;
-
-        if (isUnique(nums, nums[0])) {
-            ans = Math.max(ans, nums[0]);
-        }
-
-        if (isUnique(nums, nums[n - 1])) {
-            ans = Math.max(ans, nums[n - 1]);
-        }
-
-        return ans;
-    }
-
-    private boolean isUnique(int[] nums, int target) {
-        int count = 0;
-
-        for (int num : nums) {
-            if (num == target) {
+    public boolean unique(int[] arr,int target){
+        int count=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==target){
                 count++;
             }
         }
+        return count==1;
+    }
+    public int largestInteger(int[] nums, int k) {
+        if(k==1){
+            int ans=-1;
+            HashMap<Integer,Integer> map=new HashMap<>();
 
-        return count == 1;
+            for(int i:nums){
+                map.put(i,map.getOrDefault(i,0)+1);
+            }
+
+            for(int i:map.keySet()){
+                if(map.get(i)==1){
+                    ans=Math.max(ans,i);
+                }
+            }
+            return ans;
+        }
+
+        if(k==nums.length){
+            int ans=-1;
+            for(int i:nums){
+                ans=Math.max(ans,i);
+            }
+            return ans;
+        }
+
+        int ans=-1;
+
+        if(unique(nums,nums[0])){
+            ans=Math.max(ans,nums[0]);
+        }
+        if(unique(nums,nums[nums.length-1])){
+            ans=Math.max(ans,nums[nums.length-1]);
+        }
+
+        return ans;
     }
 }
