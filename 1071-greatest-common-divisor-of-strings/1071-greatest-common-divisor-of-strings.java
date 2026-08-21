9456054f45dd75c6876299a368e7ae1fdd980343
Kashish Verma <kashishv.1711@gmail.com>
2026-08-21 15:53:29 +0530

    Time: 1 ms (94.72%), Space: 43.6 MB (59.02%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -7,7 +7,7 @@ class Solution {
         int a = str1.length();
         int b = str2.length();
 
-        while(b != 0) {
+        while(b > 0) {
             int temp = b;
             b = a % b;
             a = temp;
