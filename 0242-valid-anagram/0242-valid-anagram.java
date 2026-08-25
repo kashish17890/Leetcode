7b8ae7597c1fabf5b5acf0cc0b4d8a7d3a93c56c
Kashish Verma <kashishv.1711@gmail.com>
2026-08-25 22:07:24 +0530

    Time: 26 ms (5.2%), Space: 46.5 MB (34.51%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if(s.length()!=t.length()) return false;
+        HashMap<Character,Integer> map1=new HashMap<>();
+        HashMap<Character,Integer> map2=new HashMap<>();
+
+        for(int i=0;i<s.length();i++){
+            map1.put(s.charAt(i),map1.getOrDefault(s.charAt(i),0)+1);
+            map2.put(t.charAt(i),map2.getOrDefault(t.charAt(i),0)+1);
+        }
+
+        for(char c:map1.keySet()){
+            if(!map1.get(c).equals(map2.get(c))) return false;
+        }
+
+        return true;
+    }
+}
