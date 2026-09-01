ae3ab9731b53b7acf4880adf00749cd850f9c54d
Kashish Verma <kashishv.1711@gmail.com>
2026-09-01 15:36:30 +0530

    Time: 13 ms (37.04%), Space: 46.5 MB (48.75%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -2,14 +2,21 @@ class Solution {
     public boolean canConstruct(String ran, String maga) {
         HashMap<Character,Integer> map=new HashMap<>();
 
-        for(int i=0;i<maga.length();i++){
-            map.put(maga.charAt(i),map.getOrDefault(maga.charAt(i),0)+1);
-        }
         for(int i=0;i<ran.length();i++){
-            char ch=ran.charAt(i);
-            if(!map.containsKey(ch) || map.get(ch)==0) return false;
-            map.put(ch,map.get(ch)-1);
+            map.put(ran.charAt(i),map.getOrDefault(ran.charAt(i),0)+1);
         }
+
+        for(int i=0;i<maga.length();i++){
+            char ch=maga.charAt(i);
+            if(map.containsKey(ch) && map.get(ch)>0){
+                map.put(ch,map.get(ch)-1);
+            }
+        }
+
+        for(int n:map.values()){
+            if(n>0) return false;
+        }
+
         return true;
     }
 }
